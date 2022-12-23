4378ebe4ca12324fa163b6f03f6c701103ebf416
Cameron Romo <cromo1225@gmail.com>
2022-12-22 22:03:51 -0700

    Add carrierwave functionality for uploading images

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to :users, notice: "Login Successful."
+      redirect_to :posts, notice: "Login Successful."
     else
       flash[:alert] = "Login Failed : ("
       render 'new', status: 403
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  before_save do
+    self.remote_image_url = 'http://placehold.it/100x100'
+  end
+
   belongs_to :user
+  mount_uploader :image, ImageUploader
+  validates :image, presence: true
 end
