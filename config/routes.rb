f2272273482b2b90e31613db05e4c01ca486fd55
Cameron Romo <cromo1225@gmail.com>
2022-12-22 21:01:36 -0700

    Track user session via id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: %i[new create destroy]
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  get 'signup', to: 'users#new'
+  get 'sessions', to: 'sessions#new'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
